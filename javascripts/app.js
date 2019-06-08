eb6caac367f8972faf9bc11f76964ae7d75db099
Ricardo Justino <ricartoons@hotmail.com>
2019-06-08 12:02:22 -0300

    [wip] - map limitation

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,10 +1,10 @@
 (function () {
   'use strict'
   //Rover
-  var roverVeicule = {
+  var rover1 = {
     direction: "N",
-    position: [0,0],
-    travelLog: []
+    position: [0, 0],
+    travelLog: [[0, 0]]
   }
 
   //Directions
@@ -38,40 +38,78 @@
 
   function moveForward(rover) {
     console.log("moveForward was called");
+    //last position
+    let lastPosition = rover.travelLog[rover.travelLog.length-1];
     //"walk"
-    if(rover.direction === "N"){
-      rover.position[1] -= 1;
-    } else if(rover.direction === "S"){
-      rover.position[1] += 1;
-    } else if(rover.direction === "E"){
-      rover.position[0] -= 1;
-    } else if(rover.direction === "W"){
-      rover.position[0] += 1;
+    if (rover.direction === "N") {
+      lastPosition[1] -= 1;
+      if (lastPosition[1] < 0) {
+        console.log("Você morreu, porque caiu da borda do mundo!");
+        return;
+      } else {
+        rover.position[1] -= 1;
+      }
+    } else if (rover.direction === "S") {
+      if (lastPosition[1] > 8) {
+        console.log("Você morreu, porque caiu da borda do mundo!");
+        return;
+      } else {
+        rover.position[1] += 1;
+      }
+    } else if (rover.direction === "E") {
+      if (lastPosition[0] > 8) {
+        console.log("Você morreu, porque caiu da borda do mundo!");
+        return;
+      } else {
+        rover.position[0] += 1;
+      }
+    } else if (rover.direction === "W") {
+      if (lastPosition[0] < 0) {
+        console.log("Você morreu, porque caiu da borda do mundo!");
+        return;
+      } else {
+        rover.position[0] -= 1;
+      }
     }
     console.log(`The Rover is ${rover.position} position now`);
-    //rover.travelLog.push([...rover.position]);
-    rover.travelLog.push([rover.position[0],rover.position[1]]);
+    rover.travelLog.push([rover.position[0], rover.position[1]]);
   }
 
-  function commandList(com){
-    for(let i = 0; i<com.length; i++){
+  function commandList(com) {
+    for (let i = 0; i < com.length; i++) {
       switch (com[i]) {
         case "r":
-          turnRight(roverVeicule);
+          turnRight(rover1);
           break;
         case "l":
-          turnLeft(roverVeicule);
-          break;  
+          turnLeft(rover1);
+          break;
         case "f":
-          moveForward(roverVeicule);
-          break;    
+          moveForward(rover1);
+          break;
         default:
           break;
       }
     }
   }
-
-  commandList("rffrfflfrff");
-  console.log(roverVeicule.travelLog);
+  // console.log('Viagem - Caminho Suave');
+  // commandList("rffrfflfrff");
+  // console.log(rover1.travelLog);
   
+  // console.log('Viagem - Caminho From Hell 1');
+  // commandList("f");
+  // console.log(rover1.travelLog);
+
+  console.log('Viagem - Caminho From Hell 2');
+  commandList("rfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrfrf");
+  console.log(rover1.travelLog);
+
+  // console.log('Viagem - Caminho From Hell 3');
+  // commandList("rfflf");
+  // console.log(rover1.travelLog);
+
+  // console.log('Viagem - Caminho From Hell 4');
+  // commandList("rfflf");
+  // console.log(rover1.travelLog);
+
 })()
